ec1619b2a6794ad9840d5e2c3f4900d16d207739
franmu94 <fmalourarte@gmail.com>
2024-03-25 11:06:50 +0100

    Initial Commit

---
+++ ApuntesPrimeraApp/ApuntesPrimeraAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApuntesPrimeraAppApp.swift
+//  ApuntesPrimeraApp
+//
+//  Created by Fran Malo on 25/3/24.
+//
+
+import SwiftUI
+
+@main
+struct ApuntesPrimeraAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
